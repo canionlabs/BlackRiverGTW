38895f45764a8a99cf9f4a625a1b4c893c7829aa
caiovictormc <caio@retake.com.br>
2019-10-29 20:31:12 -0300

    common: Create DefaultModel

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class DefaultModel(models.Model):
+    created_at = models.DateTimeField("Criado em", auto_now_add=True)
+    updated_at = models.DateTimeField("Atualizado em", auto_now=True)
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        verbose_name="Criador",
+    )
+
+    class Meta:
+        abstract = True
+
